43e834d88f2998b021fbd7bd060b615d5f77c904
Lucia Okeh <luciaokeh@Lucias-MacBook-Pro.local>
2018-09-04 18:17:02 -0400

    player animation + credits scene

---
+++ Project/Assets/scripts/flicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class flicker : MonoBehaviour {
+
+	float flickerSpeedMin = 0.1f;
+	float flickerSpeedMax = 1f;
+	float timer;
+	float stopFlickerMin = 4f;
+	float stopFlickerMax = 6f;
+	float randTimer;
+	// Use this for initialization
+	void Start () {
+		timer = 0f;
+		randTimer = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if(timer > randTimer){
+			StartCoroutine(flickerElement());
+			randTimer = Random.Range(stopFlickerMin, stopFlickerMax);
+		}
+	}
+
+	IEnumerator flickerElement(){
+		gameObject.GetComponent<Text>().enabled = false;
+		yield return new WaitForSeconds(Random.Range(flickerSpeedMin,flickerSpeedMax));
+		gameObject.GetComponent<Text>().enabled = true;
+		yield return new WaitForSeconds(Random.Range(flickerSpeedMin,flickerSpeedMax));
+		timer = 0f;
+	}
+}
